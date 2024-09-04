9a8ef22c5003b144ec8254b7f140854845c34ce3
Elango Senthilnathan <144163174+elangosenthilnathan@users.noreply.github.com>
2024-09-04 09:16:06 -0700

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,7 @@
+def connect_to_service():
+  #Hard-Coded API Key (not secure)
+  api_key = "sk_test_4eC39HqLyjWDarjT1zdp7dc"
+  print(f"Connecting to service with API Key: {api_key}")
+
+if __name__ == "__main__":
+  connect_to_service()
